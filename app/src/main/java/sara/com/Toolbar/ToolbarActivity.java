047508e23fa24c5b4443f779eccf897b02fd7ba7
bassem3312 <basem3312@gmail.com>
2016-07-11 15:07:01 +0200

    finish_toolbar

--- app/src/main/java/sara/com/Toolbar/ToolbarActivity.java
+++ app/src/main/java/sara/com/Toolbar/ToolbarActivity.java
@@ -1,8 +1,10 @@
 package sara.com.Toolbar;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 import sara.com.materialdesign.R;
 
@@ -14,12 +16,25 @@ public class ToolbarActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_toolbar);
-        SetupToolbar();
+        setToolbar();
     }
 
-    private void SetupToolbar() {
+    private void setToolbar() {
         toolbar = (Toolbar) findViewById(R.id.app_bar);
-        
         setSupportActionBar(toolbar);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setHomeButtonEnabled(true);
+        }
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
 }
--- app/src/main/java/sara/com/materialdesign/MainActivity.java
+++ app/src/main/java/sara/com/materialdesign/MainActivity.java
@@ -1,13 +1,53 @@
 package sara.com.materialdesign;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import sara.com.Toolbar.ToolbarActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setToolbar();
+    }
+
+    private void setToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.setting:
+                Toast.makeText(this, "Hi setting", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.navigation:
+                startActivity(new Intent(this, ToolbarActivity.class));
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
